79cf13ae40f541dee005e0ef7bdfccbde4369401
Abdulazeez <abdulwaliyybello@gmail.com>
2026-02-05 10:41:06 +0100

    Go live on render

--- config/sequelize.js
+++ config/sequelize.js
@@ -4,20 +4,32 @@ dotenv.config();
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize(
-    process.env.NEON_DATABASE,
-    process.env.NEON_OWNER,
-    process.env.NEON_SECRET,
-    {
-        host: process.env.NEON_HOST,
-        dialect: 'postgres',
-        dialectOptions: {
-        ssl: {
+// const sequelize = new Sequelize(
+//     process.env.NEON_DATABASE,
+//     process.env.NEON_OWNER,
+//     process.env.NEON_SECRET,
+//     {
+//         host: process.env.NEON_HOST,
+//         dialect: 'postgres',
+//         dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//         }
+//         },
+//         logging: false,
+//     }
+// );
+
+const isProduction = config.nodeEnv === 'production';
+export  const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: false,
+    dialect: 'postgres',
+    dialectOptions:{
+        ssl: isProduction? {
             require: true,
-            rejectUnauthorized: false,
-        }
-        },
-        logging: false,
+            rejectUnauthorized: false
+        } : false 
     }
-);
+})
 export default sequelize;
