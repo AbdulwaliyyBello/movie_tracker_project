a88ecdc31c567a337a707d0be0779c8f0a124293
Abdulazeez <abdulwaliyybello@gmail.com>
2026-02-11 18:53:27 +0100

    bug fix

--- middleware/uservalidator.js
+++ middleware/uservalidator.js
@@ -4,6 +4,8 @@ import { User } from "../db/models/user.js";
 
 export const verifyUser = async (req, res, next) =>{
     try {
+        console.log(req)
+        console.log(req.authorization)
         const jwtHeader = req.authorization;
         if(!jwtHeader)  return res.status(401).json({Error: 'Missing JWT. Please Log in'})
         
