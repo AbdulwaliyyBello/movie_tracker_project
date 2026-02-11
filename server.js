2ccfd839590de31ee3b33f3e5c005202dfe8a1d5
Abdulazeez <abdulwaliyybello@gmail.com>
2026-02-11 16:20:19 +0100

    bug fixes 2.0

--- server.js
+++ server.js
@@ -3,7 +3,20 @@ import { config } from './config/env.js';
 import { initDB } from './db/models/index.js';
 import { router } from './routes/movies.js';
 import {router as auth} from './routes/user_auth.js'
+import sequelize from './config/sequelize.js';
 const PORT  = process.env.PORT || 4500;
+
+sequelize.sync()
+    .then(() => {
+        console.log("Database synced successfully");
+        app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Database sync failed:", err);
+    });
+
 const app = express()
 app.use(express.json())
 app.use("/movies", router)
