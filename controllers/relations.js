3d3b5ebee62c076893cc021eeb3a0d50e904dcde
Abdulazeez <abdulwaliyybello@gmail.com>
2026-02-12 12:43:57 +0100

    Final working model 2

--- controllers/relations.js
+++ controllers/relations.js
@@ -1,17 +1,18 @@
 import { watched } from "../db/models/watched.js";
+import { To_watch } from "../db/models/to_watch.js";
 import { movies } from "../db/models/movies.js";
-import { feedback } from "../db/models/feedback.js"
+import { feedback } from "../db/models/feedback.js";
+
 
 watched.belongsTo(movies, {
     foreignKey: "movieId",
     targetKey: "imdbID"
-    });
+});
 
 movies.hasMany(watched, {
     foreignKey: "movieId",
     sourceKey: "imdbID"
 });
-
 
 watched.hasOne(feedback, {
     foreignKey: "imdbID",
@@ -23,3 +24,13 @@ feedback.belongsTo(watched, {
     targetKey: "movieId"
 });
 
+
+To_watch.belongsTo(movies, {
+    foreignKey: "movieId",
+    targetKey: "imdbID"
+});
+
+movies.hasMany(To_watch, {
+    foreignKey: "movieId",
+    sourceKey: "imdbID"
+});
