1d65aed081b35522c1a23d5c21b614e4729fc947
Abdulazeez <abdulwaliyybello@gmail.com>
2026-02-12 12:01:36 +0100

    new routes

--- routes/movies.js
+++ routes/movies.js
@@ -5,6 +5,7 @@ import {getAllWaitlist, wantToWatch} from "../controllers/wantTowatch.js"
 import { addToWatched } from '../controllers/watched.js';
 import { getDashboardStats } from '../controllers/dashboard.js';
 import { addFeedback, getUserRatings } from '../controllers/feedback..js';
+import { filter } from '../controllers/filter.js';
 export const router = express.Router()
 
 router.get("/getShows", verifyUser, searchMovie)
@@ -16,3 +17,4 @@ router.get("/getAllWaitlist", verifyUser, getAllWaitlist)
 router.post("/feedback/addreview", verifyUser, addFeedback)
 router.get("/getuserratings", verifyUser, getUserRatings)
 router.get("/stats", verifyUser, getDashboardStats)
+router.get("/filterSearch", verifyUser, filter)
